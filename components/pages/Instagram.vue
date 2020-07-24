74cba7f327e39d1f6d797bd2b0416e54813586cf
bmb-design92 <35960774+xolms111@users.noreply.github.com>
2020-07-24 11:57:14 +0300

    123

--- components/pages/Instagram.vue
+++ components/pages/Instagram.vue
@@ -13,10 +13,34 @@
                 </p>
               </div>
               <div class="instagram__link">
-                <a href="">перейти</a>
+                <a :href="setting.social.instagram.link" target="_blank">
+                  перейти
+                  <svg
+                    width="32"
+                    height="24"
+                    viewBox="0 0 32 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M24.0909 7L23.1616 7.94L26.8393 11.6667L3 11.6667L3 13L26.8393 13L23.1616 16.7267L24.0909 17.6667L29.3636 12.3333L24.0909 7Z"
+                      fill="white"
+                    />
+                  </svg>
+                </a>
               </div>
             </div>
-            <div class="instagram__right"></div>
+            <div class="instagram__right">
+              <div
+                class="instagram__item"
+                v-for="(item, index) in inst"
+                :key="'instagram_' + index"
+              >
+                <a :href="item.link" target="_blank">
+                  <img v-lazy="item.image" :alt="item.link" />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -24,5 +48,30 @@
   </section>
 </template>
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+export default {
+  data() {
+    return {
+      inst: [
+        {
+          link: '/123',
+          image: '/instagram/1.jpg'
+        },
+        {
+          link: '/123',
+          image: '/instagram/1.jpg'
+        },
+        {
+          link: '/123',
+          image: '/instagram/1.jpg'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters({
+      setting: 'getSetting'
+    })
+  }
+}
 </script>
